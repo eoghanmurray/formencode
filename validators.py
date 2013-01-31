25ff1921eea70b99263d26dc974398ca6784df71
Eoghan Murray <eoghan@getthere.ie>
2013-01-31 16:01:36 +0000

    Disallow double quotes in email addresses; stricter email validation based on https://github.com/noelbush/py_email_validation/

--- validators.py
+++ validators.py
@@ -1242,6 +1242,49 @@ class Set(FancyValidator):
         else:
             return []
 
+# From https://github.com/noelbush/py_email_validation/
+WSP = r'[ \t]'                                       # see 2.2.2. Structured Header Field Bodies
+CRLF = r'(?:\r\n)'                                   # see 2.2.3. Long Header Fields
+NO_WS_CTL = r'\x01-\x08\x0b\x0c\x0f-\x1f\x7f'        # see 3.2.1. Primitive Tokens
+QUOTED_PAIR = r'(?:\\.)'                             # see 3.2.2. Quoted characters
+FWS = r'(?:(?:' + WSP + r'*' + CRLF + r')?' + \
+      WSP + r'+)'                                    # see 3.2.3. Folding white space and comments
+CTEXT = r'[' + NO_WS_CTL + \
+        r'\x21-\x27\x2a-\x5b\x5d-\x7e]'              # see 3.2.3
+CCONTENT = r'(?:' + CTEXT + r'|' + \
+           QUOTED_PAIR + r')'                        # see 3.2.3 (NB: The RFC includes COMMENT here
+                                                     # as well, but that would be circular.)
+COMMENT = r'\((?:' + FWS + r'?' + CCONTENT + \
+          r')*' + FWS + r'?\)'                       # see 3.2.3
+CFWS = r'(?:' + FWS + r'?' + COMMENT + ')*(?:' + \
+       FWS + '?' + COMMENT + '|' + FWS + ')'         # see 3.2.3
+ATEXT = r'[\w!#$%&\'\*\+\-/=\?\^`\{\|\}~]'           # see 3.2.4. Atom
+ATOM = CFWS + r'?' + ATEXT + r'+' + CFWS + r'?'      # see 3.2.4
+DOT_ATOM_TEXT = ATEXT + r'+(?:\.' + ATEXT + r'+)*'   # see 3.2.4
+DOT_ATOM = CFWS + r'?' + DOT_ATOM_TEXT + CFWS + r'?' # see 3.2.4
+QTEXT = r'[' + NO_WS_CTL + \
+        r'\x21\x23-\x5b\x5d-\x7e]'                   # see 3.2.5. Quoted strings
+QCONTENT = r'(?:' + QTEXT + r'|' + \
+           QUOTED_PAIR + r')'                        # see 3.2.5
+QUOTED_STRING = CFWS + r'?' + r'"(?:' + FWS + \
+                r'?' + QCONTENT + r')*' + FWS + \
+                r'?' + r'"' + CFWS + r'?'
+LOCAL_PART = r'(?:' + DOT_ATOM + r'|' + \
+             QUOTED_STRING + r')'                    # see 3.4.1. Addr-spec specification
+DTEXT = r'[' + NO_WS_CTL + r'\x21-\x5a\x5e-\x7e]'    # see 3.4.1
+DCONTENT = r'(?:' + DTEXT + r'|' + \
+           QUOTED_PAIR + r')'                        # see 3.4.1
+DOMAIN_LITERAL = CFWS + r'?' + r'\[' + \
+                 r'(?:' + FWS + r'?' + DCONTENT + \
+                 r')*' + FWS + r'?\]' + CFWS + r'?'  # see 3.4.1
+DOMAIN = r'(?:' + DOT_ATOM + r'|' + \
+         DOMAIN_LITERAL + r')'                       # see 3.4.1
+ADDR_SPEC = LOCAL_PART + r'@' + DOMAIN               # see 3.4.1
+
+# A valid address will match exactly the 3.4.1 addr-spec.
+VALID_ADDRESS_REGEXP = '^' + ADDR_SPEC + '$'
+
+
 class Email(FancyValidator):
     r"""
     Validate an email address.
@@ -1276,6 +1319,10 @@ class Email(FancyValidator):
         Traceback (most recent call last):
             ...
         Invalid: The domain portion of the email address is invalid (the portion after the @: .foo.bar.com)
+        >>> e.to_python('test"@foo.com')
+        Traceback (most recent call last):
+            ...
+        Invalid: The username portion of the email address is invalid (the portion before the @: test")
         >>> e.to_python('nobody@xn--m7r7ml7t24h.com')
         'nobody@xn--m7r7ml7t24h.com'
         >>> e.to_python('o*reilly@test.com')
@@ -1301,11 +1348,8 @@ class Email(FancyValidator):
 
     resolve_domain = False
 
-    usernameRE = re.compile(r"^[^ \t\n\r@<>()]+$", re.I)
-    domainRE = re.compile(r'''
-        ^(?:[a-z0-9][a-z0-9\-]{0,62}\.)+ # (sub)domain - alpha followed by 62max chars (63 total)
-        [a-z]{2,}$                       # TLD
-    ''', re.I | re.VERBOSE)
+    usernameRE = re.compile(r"^"+LOCAL_PART+"$", re.I)
+    domainRE = re.compile(r"^"+DOMAIN+"$", re.I)
 
     messages = {
         'empty': _('Please enter an email address'),
